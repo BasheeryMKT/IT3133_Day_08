877f6e48ecab8ba65de55ccb26a7c0be095ea18b
BasheeryMKT <154669750+BasheeryMKT@users.noreply.github.com>
2024-11-26 08:32:01 +0000

    Updates

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import AboutUs from './components/AboutUs';
+import Home from './components/Home';
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ContactUs/>
+      <AboutUs/>
+      <Home/>
+
     </div>
+    
   );
 }
 
