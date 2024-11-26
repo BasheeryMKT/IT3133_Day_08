46c21922b87e37a2ddde78c1239136b83f113e96
BasheeryMKT <154669750+BasheeryMKT@users.noreply.github.com>
2024-11-26 09:55:31 +0000

    Updated

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom"
 export default function Login(){
+    const username = setUsername();
     const navigate = useNavigate();
     const authenticate=()=>{
         navigate('/dashboard')
@@ -9,6 +10,14 @@ export default function Login(){
             <h1>Login Here</h1>
             
             <button onClick={authenticate}>Login</button>
+
+            <form>
+                <input type='text'
+                value={username}
+                onChange={(e)=>setUsername(e.target.value)}
+                placeholder="username"/> 
+                
+            </form>
         </div>
     )
 }
